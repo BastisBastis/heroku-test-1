4a418bb4fb6fe6fdb95b09308d60545643ed9ebc
BastisBastis <65193082+BastisBastis@users.noreply.github.com>
2020-05-16 05:47:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/', function (req, res) {
+  res.send('hello world')
+})
+ 
+
+.get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/db', async (req, res) => {
     try {
